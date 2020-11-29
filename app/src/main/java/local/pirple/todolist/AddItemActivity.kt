26b8b100c8366987011f7cfc9a8e8d9553414c99
Dyrits <dyrits@outlook.com>
2020-11-29 14:28:30 +0100

    Initial commit. README not available.

---
+++ app/src/main/java/local/pirple/todolist/AddItemActivity.kt
@@ -0,0 +1,11 @@
+package local.pirple.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+    }
+}
